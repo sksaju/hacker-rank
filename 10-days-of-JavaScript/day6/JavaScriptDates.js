1ff4c4c48ce4abd8b778794300c22af03a907c90
unknown <sksaaju@gmail.com>
2018-10-11 22:36:38 -0700

    Day6 solutions added

---
+++ 10-days-of-JavaScript/day6/JavaScriptDates.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    var dateString = new Date(dateString);
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    dayName = weekday[dateString.getDay()];
+    
+    return dayName;
+}
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
